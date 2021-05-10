56d9a760527b6a6bf9cc0d44a0c7d0cc913082c3
JBR90 <blakeage77@googlmail.com>
2021-05-10 22:05:28 +0100

    Webpack setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// import your function
+import myName from "./myName";
+import { functionOne, functionTwo } from "./example_export_multiple";
+
+// Dynamic element creation
+function component() {
+  const element = document.createElement("div");
+
+  // use imported myName function!
+  element.innerHTML = myName("Joe");
+  return element;
+}
+
+document.body.appendChild(component());
+
+const test = document.getElementById("test");
+
+test.innerHTML = functionOne();
