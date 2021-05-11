77a3a73ba403b10990ee2e8118e65ddf215c3cff
Peter Curcio <pcurcio988@gmail.com>
2021-05-11 07:45:32 -0700

    adding initial api structure

--- js/api.js
+++ js/api.js
@@ -1,1 +1,6 @@
 // Google Books API
+const API_KEY = "AIzaSyCJilO37UK591gPqByiXK6VoUB-ZBdPIV0";
+const URL = "https://www.googleapis.com/books/v1/volumes";
+const SEARCH_STRING;
+const SEARCH_QUERY = URL + "?=" + SEARCH_STRING + "key=" + API_KEY;
+//https://www.googleapis.com/books/v1/volumes?q=atomic+habits&key=AIzaSyCJilO37UK591gPqByiXK6VoUB-ZBdPIV0
