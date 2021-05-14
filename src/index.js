f028f75dba9adcd94c11fe7766051d17eddf5dd4
Peter Curcio <pcurcio988@gmail.com>
2021-05-14 08:16:24 -0700

    1) added url-loader to webpack config (for image path resolution), 2) and added assets/images folder structure

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const searchInput = document.getElementById("search-input");
 // Event Listener
 searchInput.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    return getSearchResults(); 
+    return getSearchResults();
   }
 });
 
