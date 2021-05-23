0db8e99a2d88276358f9f0ebe8527c29db79e82c
bb <>
2021-05-23 23:24:02 +0100

    Updated SCSS structure

--- src/api.js
+++ src/api.js
@@ -2,19 +2,23 @@ import renderCard from "./bookCard";
 import searchedBooks from "./searchedBooks";
 
 // Google Books API
-const API_KEY = process.env.GOOGLE_BOOKS_API_KEY;
+const API_KEY =
+  process.env.GOOGLE_BOOKS_API_KEY || "AIzaSyAH8UeJed2tdWtyZFxYV2ke2GmfJ1xgbMc"; // just for dev;
 const GOOGLE_BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes";
 
 async function getSearchResults() {
   const bookCardContainer = document.querySelector(".container");
   bookCardContainer.innerHTML = "";
-  const searchValue = document.getElementById("search-input").value.split(" ").join("+");
+  const searchValue = document
+    .getElementById("search-input")
+    .value.split(" ")
+    .join("+");
   const searchQuery = `${GOOGLE_BOOKS_API_URL}?q=${searchValue}&key=${API_KEY}&maxResults=25`;
 
   try {
     const response = await fetch(searchQuery);
     //console.log("response", response);
-  
+
     if (!response.ok) {
       const message = `An error has occurred: ${response.status}`;
       throw new Error(message);
@@ -32,7 +36,6 @@ async function getSearchResults() {
     });
 
     //return data;
-
   } catch (e) {
     console.log(e.message);
     console.log(e.details);
